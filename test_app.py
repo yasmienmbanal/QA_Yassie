65f783270343ef30288087c166f7b117a95bfc33
johnron <dichosojohnron34@gmail.com>
2025-04-11 19:26:08 +0800

    update

--- test_app.py
+++ test_app.py
@@ -9,14 +9,13 @@ def test_categorize_numbers_empty():
 def test_categorize_numbers_mixed():
     evens, odds = categorize_numbers([1, 2, 3, 4, 5, 6])
     assert evens == [2, 4, 6]
-    assert odds == [1, 3, 5]
+    assert odds == [1, 3, 7, 9]
 
 
 def test_categorize_numbers_all_even():
     evens, odds = categorize_numbers([2, 4, 6, 8])
     assert evens == [2, 4, 6, 8]
     assert odds == []
-
 
 def test_categorize_numbers_all_odd():
     evens, odds = categorize_numbers([1, 3, 5, 7])
